58934e3903c152fff76afa3f892509e5290c3d47
krishna <krishnaof1988@gmail.com>
2015-08-05 11:42:09 +0530

    drag and drop initial integration

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,3 +18,42 @@ var minimise = function() {
 var closeApp = function() {
   win.close();
 };
+
+var toggleViews = function() {
+  var step_1 = $('#step-1');
+  var step_2 = $('#step-2');
+  if (step_1.hasClass('hide')) {
+    step_2.addClass('hide');
+    step_1.removeClass('hide');
+  } else {
+    step_1.addClass('hide');
+    step_2.removeClass('hide');
+  }
+};
+
+var UploadListener = function() {
+  var holder;
+
+  var handler = function (e) {
+    e.preventDefault();
+    var file;
+    var filesLog = "";
+    for (var i = 0; i < e.dataTransfer.files.length; ++i) {
+      file = e.dataTransfer.files[i];
+      filesLog += (file.name + " : type - " + file.type + "\n");
+    }
+    alert(filesLog);
+    return false;
+  };
+
+  this.init = function(id) {
+    holder = document.getElementById(id);
+    holder.ondragover = function () { this.className = 'hover'; return false; };
+    holder.ondragleave = function () { this.className = ''; return false; };
+    holder.ondrop = handler;
+  };
+
+  return this;
+};
+
+UploadListener().init("drag_drop");

96e278c1febe2f62fa03692f709a54e6b94ed1c5
krishna <krishnaof1988@gmail.com>
2015-08-07 17:32:11 +0530

    UI integration interim commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 var gui = require('nw.gui');
 var win = gui.Window.get();
+var package_config = require('../package.json');
 
 // Info related to the UI can be added to the windowState
 var windowState = {
@@ -19,6 +20,38 @@ var closeApp = function() {
   win.close();
 };
 
+var showError = function(id, msg) {
+  var pos = $('#' + id).offset();
+  var errorElement = $('.error');
+  errorElement.css('top', pos.top + 40 + 'px');
+  errorElement.css('left', pos.left + 'px');
+  errorElement.css('display', 'block');
+  $('.error-msg').html(msg);
+  setTimeout(function() {
+    $('.error').css('display', 'none');
+  }, 3000);
+};
+
+var validate = function() {
+  var serviceName = document.getElementById('service_name').checkValidity();
+  var publicName = document.getElementById('public_name').checkValidity();
+  if (serviceName && publicName) {
+    return true;
+  } else if(!serviceName) {
+    showError('service_name', 'Service Name should be filled');
+  } else if(!publicName) {
+    showError('public_name', 'Public Name should be filled');
+  }
+  return false;
+};
+
+var register = function() {
+  if(!validate()) {
+    return;
+  }
+  toggleViews();
+};
+
 var toggleViews = function() {
   var step_1 = $('#step-1');
   var step_2 = $('#step-2');
@@ -30,6 +63,11 @@ var toggleViews = function() {
     step_2.removeClass('hide');
   }
 };
+
+if (package_config.window.frame) {
+  $('header').remove();
+  $('body').css('border-width', '0px');
+}
 
 /**
  *  Dragover and drop is disabled on document.
@@ -44,3 +82,4 @@ document.addEventListener('drop', function(e){
   e.preventDefault();
   e.stopPropagation();
 }, false);
+
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 ## dns_manager
 
-Sample Application
+An example application for uploading files to the Safe Network.
+
+#Pre-Requisites:
+nodejs should be installed.
+The application is built using [nwjs](https://github.com/nwjs/nw.js). For development nwjs should be setup locally.
+
+1. Clone the project
+2. execute `grunt init`. This will download and initialise dependencies for the application development
